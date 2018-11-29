dc6a478604e81025d644f244d017bcb3292fa1f3
ak0-hal-ns <ak0.hal.ns@gmail.com>
2018-11-29 12:46:58 +0900

    Implement main loop

--- main.asm
+++ main.asm
@@ -1,5 +1,6 @@
     global _start
 
+    %include "util.inc"
     %include "macro.inc"
     %include "words.inc"
 
@@ -10,12 +11,30 @@ input_buf:
     resb 1024
 
 
+    section .rodata
+msg_err_find:
+    db ":no such word in the dictionary.", 10, 0
+msg_test:
+    db "Hello, world!", 10, 0
+
+
     section .data
 
 last_word:
     dq _last_word
 
 prog_stub:
+.loop:
+    dq xt_inbuf
+    dq xt_word
+    dq xt_drop
+    dq xt_inbuf
+    dq xt_find
+    dq xt_branch_end
+    dq xt_wh2xt
+    dq xt_execute
+    dq xt_loop
+.end:
     dq xt_exit
 
 
@@ -23,7 +42,7 @@ prog_stub:
 
 next:
     mov xt, [pc]
-    add pc, 8
+    add pc, cellsize
     jmp [xt]
 
 _start:
